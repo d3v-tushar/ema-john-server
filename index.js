8495f6db53a8d349b50eec12793aeebdb9e66b8a
d3v-tushar <d3v.tushar@gmail.com>
2022-11-19 05:43:54 +0600

    Database Skip & Limit             const size = parseInt(req.query.size);

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@learnph.159fxoq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async() =>{
+    try{
+        const productCollection = client.db("emaJohn").collection("products");
+        app.get('/products', async(req, res) =>{
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.skip(page*size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count, products});
+        });
+    }
+    finally{
+        // client.close();
+    }
+};
+run().catch(error => console.error(error));
+
+
+app.get('/', (req, res) =>{
+    res.send('Ema-John Server Is Running');
+});
+
+app.listen(port, () =>{
+    console.log(`Ema-John Server is running on port: ${port}`);
+});
